11d623695502ade6517f118101665d8bb34e8fa6
secona <77039267+secona@users.noreply.github.com>
2024-08-29 01:49:27 +0700

    grub settings

--- configuration.nix
+++ configuration.nix
@@ -12,9 +12,10 @@
   boot.loader = {
     grub = {
       enable = true;
-      device = "nodev";
       useOSProber = true;
+      devices = [ "nodev" ];
       efiSupport = true;
+      configurationLimit = 5;
     };
 
     efi.canTouchEfiVariables = true;
