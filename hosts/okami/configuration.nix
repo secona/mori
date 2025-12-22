d87bbd5c8ed0945fb17bc792bcb0e9bc0752c8b5
Vito Secona <secona00@gmail.com>
2025-12-22 10:40:40 +0700

    add steam

--- hosts/okami/configuration.nix
+++ hosts/okami/configuration.nix
@@ -226,6 +226,13 @@
     niri.enable = true;
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
   powerManagement = {
     enable = true;
   };
