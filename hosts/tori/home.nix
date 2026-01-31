291ed2429853cad798542903a6bef492a3a72f5f
Vito Secona <secona00@gmail.com>
2026-01-31 21:24:55 +0700

    [tori] enable modules

--- hosts/tori/home.nix
+++ hosts/tori/home.nix
@@ -13,5 +13,30 @@
     stateVersion = "25.11";
   };
 
+  cli.shells.fish.enable = true;
+  cli.tools.tmux.enable = true;
+  cli.tools.starship.enable = true;
+  cli.tools.eza.enable = true;
+
+  dev.editors.nvim.enable = true;
+  dev.tools.direnv.enable = true;
+
+  dev.vcs =
+    let
+      userName = "Vito Secona";
+      userEmail = "secona00@gmail.com";
+    in
+    {
+      git = {
+        enable = true;
+        inherit userName userEmail;
+      };
+
+      jujutsu = {
+        enable = true;
+        inherit userName userEmail;
+      };
+    };
+
   desktop.ui.catppuccin.enable = true;
 }
