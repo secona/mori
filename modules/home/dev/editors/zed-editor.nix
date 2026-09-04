3fd9340f3ec2e3f516be1668049254b05210da20
Vito Secona <secona00@gmail.com>
2026-09-04 09:20:06 +0700

    [zed] use php 8.4 and composer

--- modules/home/dev/editors/zed-editor.nix
+++ modules/home/dev/editors/zed-editor.nix
@@ -17,7 +17,8 @@
       enable = true;
       package = pkgs.unstable.zed-editor;
       extraPackages = with pkgs.unstable; [
-        php
+        php84
+        php84Packages.composer
         phpactor
       ];
       extensions = [
